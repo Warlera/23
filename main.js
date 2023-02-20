0fc6201fe620dcbf34e59f43c854497b1f7810a7
Warlera <90709624+Warlera@users.noreply.github.com>
2023-02-20 17:46:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -49,3 +49,14 @@ function nextSong() {
     playSong()
 })
 file.addEventListener('ended', nextSong)
+let glowInTexts = document.querySelectorAll(".glowIn");
+glowInTexts.forEach(glowInText => {
+  let letters = glowInText.textContent.split("");
+  glowInText.textContent = "";
+  letters.forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = `${i * 0.05}s`;
+    glowInText.append(span);
+  });
+});
